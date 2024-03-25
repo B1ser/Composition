62db767999b8173826ab90e4b771a55d626dd384
B1ser <aviarezhym@gmail.com>
2024-03-25 16:55:26 +0500

    Create fragments and init ViewBinding into current classes

---
+++ app/src/main/java/com/example/composition/domain/entity/Question.kt
@@ -0,0 +1,10 @@
+package com.example.composition.domain.entity
+
+class Question (
+    //фактическое число
+    val sum : Int,
+    //первое число
+    val visibleNumbers : Int,
+    //варианты второго числа
+    val options : List<Int>
+)
